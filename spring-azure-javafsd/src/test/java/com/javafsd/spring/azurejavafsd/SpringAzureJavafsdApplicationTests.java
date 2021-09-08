58e93a412e1dcabca407cfe70d044591410b3fa3
pranalic12 <pranali.chavan@lntinfotech.com>
2021-09-08 15:59:27 +0530

    first commit

---
+++ spring-azure-javafsd/src/test/java/com/javafsd/spring/azurejavafsd/SpringAzureJavafsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafsd.spring.azurejavafsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureJavafsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
